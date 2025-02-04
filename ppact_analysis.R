1e2be8a4365a5ea3809f6e2a69110597db01a846
laramaleyeff1 <69804522+laramaleyeff1@users.noreply.github.com>
2025-02-03 20:14:28 -0500

    add simulation details and update ppact analysis

--- ppact_analysis.R
+++ ppact_analysis.R
@@ -3,149 +3,346 @@ library(dplyr)
 library(lme4)
 library(xtable)
 library(hettx)
+library(tidyr)
+library(ggplot2)
 
-R = 2000
+# ====================================================
+# Load required data and functions
+# ====================================================
+
+R = 2000  # Number of permutations
 
 # Set working directory to source file location
 source("./permutation_test_crt.R")
 
-# Download "ppact_public_bpi.sas7bdat" from https://github.com/PainResearch/PPACT
-# and load here:
-#     > data <- read_sas("ppact_public_bpi.sas7bdat")
+# Load the dataset
+data <- read_sas("../ppact_public_bpi.sas7bdat")
+# The dataset "ppact_public_bpi.sas7bdat" can be downloaded from:
+# https://github.com/PainResearch/PPACT
+# Example usage:
+# data <- read_sas("ppact_public_bpi.sas7bdat")
 
+
+# ====================================================
+# Data Preprocessing: Filtering and Selection
+# ====================================================
+
+# Filter data for timepoint 12 and select relevant variables
 data_12_cov = data %>%
-  filter(TIMEPOINT == 12) %>%
-  dplyr::select(PEGS,
-                INTERVENTION,
-                CLUST,
-                AGE,
-                FEMALE,
-                disable,
-                Current_Smoke, 
-                BMI, 
-                Alcohol_Abuse,
-                Drug_Abuse,
-                Diabetes,
-                CVD, 
-                Hypertension, 
-                Pulmonary,
-                Dep_OR_Anx,
-                pain1:pain11,
-                BL_avg_daily,
-                BL_benzo_flag
-                ) %>%
-  na.omit()
+  filter(TIMEPOINT == 12) %>%  # Keep only rows where TIMEPOINT is 12
+  dplyr::select(PEGS, INTERVENTION, CLUST,  # Key variables
+                AGE, FEMALE, disable, Current_Smoke, BMI,  # Covariates
+                Alcohol_Abuse, Drug_Abuse, Diabetes, CVD,
+                Hypertension, Pulmonary, Dep_OR_Anx,
+                pain1:pain11,  # Pain variables
+                BL_avg_daily,  # Baseline daily average
+                BL_benzo_flag   # Baseline benzodiazepine use flag
+  ) %>%
+  na.omit()  # Remove rows with missing values
 
+# Exclude individuals with extreme baseline average daily values (greater than 200)
+data_12_cov <- data_12_cov[data_12_cov$BL_avg_daily < 200,]
+
+# Aggregate data at the cluster level by computing the mean of all variables except cluster ID
 data_12_cov_cl_level = data_12_cov %>%
   group_by(CLUST) %>%
-  summarise_at(vars(-group_cols()), mean) %>%
-  ungroup()
+  summarise_at(vars(-group_cols()), mean) %>%  # Compute mean for each cluster
+  ungroup()  # Remove grouping
 
+
+# ====================================================
+# Permutation Test for Treatment Effect Heterogeneity (Ding et al.)
+# ====================================================
+
+# Set seed for reproducibility
 set.seed(123)
+
+# Conduct unadjusted cluster-level test
 ding_unadj = detect_idiosyncratic(PEGS ~ INTERVENTION, data_12_cov_cl_level, B=2000)
-# FRT CI Test for Treatment Effect Heterogeneity 
-# Statistic P-Value (Sweep) P-Value (Plug-In)
-# 1 0.1320755          0.4946            0.4356
 
-formula = paste0("PEGS ~ ", paste(names(data_12_cov_cl_level)[-c(1:3)],collapse = "+"))
-set.seed(123)
-model <- lm(as.formula(formula),data = data_12_cov_cl_level[data_12_cov_cl_level$INTERVENTION==0,])
-mod_mat = model.matrix(as.formula(formula), data=data_12_cov_cl_level)
+# Output:
+# Statistic           P-Value (Sweep)     P-Value (Plug-In)
+# 1 0.09433962        0.9276              0.9161
+
+
+# ====================================================
+# Adjusted Permutation Test for Treatment Effect Heterogeneity
+# ====================================================
+
+# Construct regression formula excluding PEGS, INTERVENTION, CLUST
+formula = paste0("PEGS ~ ", paste(names(data_12_cov_cl_level)[-c(1:3)], collapse = "+"))
+
+# Fit a linear model using only the control group (INTERVENTION == 0)
+model <- lm(as.formula(formula), data = data_12_cov_cl_level[data_12_cov_cl_level$INTERVENTION == 0,])
+
+# Construct model matrix
+mod_mat = model.matrix(as.formula(formula), data = data_12_cov_cl_level)
+
+# Compute residuals by subtracting predicted values from observed PEGS values
 data_12_cov_cl_level$resids = c(data_12_cov_cl_level$PEGS - mod_mat %*% coef(model))
 
-ding_adj = detect_idiosyncratic(resids ~ INTERVENTION, 
-                                data_12_cov_cl_level, B=2000)
-# FRT CI Test for Treatment Effect Heterogeneity 
-# Statistic P-Value (Sweep) P-Value (Plug-In)
-# 1 0.2264151          0.0321            0.0236
+# Conduct adjusted test
+set.seed(123)
+ding_adj = detect_idiosyncratic(resids ~ INTERVENTION, data_12_cov_cl_level, B=2000)
 
-# 
-# Permutation test analysis
-#
-X = data_12_cov[,-c(1,2,3)]
+# Output:
+# Statistic           P-Value (Sweep)     P-Value (Plug-In)
+# 1 0.2075472         0.0376              0.0296
+
+
+# ====================================================
+# Permutation Tests Based on GK-S and SK-S
+# ====================================================
+
 set.seed(1234)
-perm.test.adj = runPermutationTest(data_12_cov$PEGS,data_12_cov$INTERVENTION, data_12_cov$CLUST, 
-                   X[,c(1,5,24)],X[,-c(1,5,24)],adj=TRUE,R=R)
-set.seed(5678)
-perm.test.unadj = runPermutationTest(data_12_cov$PEGS,data_12_cov$INTERVENTION, data_12_cov$CLUST,
-                   R=R)
+perm.test.adj = runPermutationTest(data_12_cov$PEGS,
+                                   data_12_cov$INTERVENTION,
+                                   data_12_cov$CLUST,
+                                   data_12_cov[,c("AGE", "BL_avg_daily")],
+                                   data_12_cov[,c("disable",
+                                                  "Diabetes",
+                                                  "Hypertension",
+                                                  paste0("pain",c(3,5,11))
+                                   )], adj=TRUE, R=R)
 
+set.seed(5678)
+perm.test.unadj = runPermutationTest(data_12_cov$PEGS,
+                                     data_12_cov$INTERVENTION,
+                                     data_12_cov$CLUST, R=R)
+
+# Outputs:
 perm.test.adj
 # $p.value.ci
-# [1] 0.00159925
-
+# [1] 0.03258376
 # $p.value.pi
-# [1] 0.0005997501
+# [1] 0.02758626
 
 perm.test.unadj
 # $p.value.ci
-# [1] 0.09505252
-# 
+# [1] 0.1265368
 # $p.value.pi
-# [1] 0.0170915
+# [1] 0.03358326
 
 
-# 
-# Likelihood ratio test analysis
-#
+# ====================================================
+# Likelihood Ratio Test Analysis
+# ====================================================
+
+# Fit null model
 model_null <- lmer(PEGS ~ INTERVENTION + AGE + FEMALE + disable +
                      Current_Smoke + BMI + Alcohol_Abuse +
                      Drug_Abuse  + Diabetes +
-                     CVD + Hypertension + 
+                     CVD + Hypertension +
                      Pulmonary + Dep_OR_Anx +
                      pain1 + pain2 + pain3 + pain4 +
-                     pain5 + pain6 + pain7 + pain8 + 
-                     pain9 + pain10 + pain11 + BL_avg_daily + BL_benzo_flag + 
-                     (1|CLUST),data=data_12_cov)
-model_full_combined <- lmer(PEGS ~ INTERVENTION*(AGE + FEMALE + disable +
-                                         Current_Smoke + BMI + Alcohol_Abuse +
-                                         Drug_Abuse  + Diabetes +
-                                         CVD + Hypertension + 
-                                         Pulmonary + Dep_OR_Anx +
-                                         pain1 + pain2 + pain3 + pain4 +
-                                         pain5 + pain6 + pain7 + pain8 + 
-                                         pain9 + pain10 + pain11 + BL_avg_daily + 
-                                         BL_benzo_flag ) + 
-                              (1|CLUST),data=data_12_cov)
+                     pain5 + pain6 + pain7 + pain8 +
+                     pain9 + pain10 + pain11 + BL_avg_daily + BL_benzo_flag +
+                     (1|CLUST), data=data_12_cov)
 
-# Combined individual p-values
-p_vals_combined = car::Anova(model_full_combined, type=3)[["Pr(>Chisq)"]][28:52]
-# Combined test p-values
+# Fit full interaction model
+model_full_combined <- lmer(PEGS ~ INTERVENTION * (AGE + FEMALE + disable +
+                                                     Current_Smoke + BMI + Alcohol_Abuse +
+                                                     Drug_Abuse  + Diabetes +
+                                                     CVD + Hypertension +
+                                                     Pulmonary + Dep_OR_Anx +
+                                                     pain1 + pain2 + pain3 + pain4 +
+                                                     pain5 + pain6 + pain7 + pain8 +
+                                                     pain9 + pain10 + pain11 + BL_avg_daily +
+                                                     BL_benzo_flag) +
+                              (1|CLUST), data=data_12_cov)
+
+# Perform likelihood ratio test
 anova(model_full_combined, model_null)
 
-# Individual model p-values
-cov_list = names(data_12_cov)[-c(1,2,3)]
-p_vals_ind = rep(NA, length(cov_list))
-for (i in 1:length(cov_list)) {
-  em = data_12_cov[[cov_list[i]]] 
-  model_null = lmer(PEGS ~ INTERVENTION + em + (1|CLUST),data=data_12_cov,REML=FALSE)
-  model_full = lmer(PEGS ~ INTERVENTION*em + (1|CLUST),data=data_12_cov,REML=FALSE)
-  p_vals_ind[i] = anova(model_full, model_null)[["Pr(>Chisq)"]][2]
-}
-cov_names = c("Age, y",
-              "Sex",
-              "Receives disability benefits",
-              "Current smoker",
-              "BMI",
-              "Alcohol misuse",
-              "Drug misuse", 
-              "Diabetes",
-              "Cardiovascular disorder",
-              "Hypertension",
-              "Chronic pulmonary disease",
-              "Anxiety or depression diagnosis",
-              "Back and/or neck pain diagnosis",
-              "General pain diagnosis",
-              "Limb/extremity pain, joint pain and/or arthritic disorders diagnosis",
-              "Neuropathy diagnosis",
-              "Abdominal and/or bowel pain diagnosis",
-              "Musculoskeletal chest pain diagnosis",
-              "Urogenital, pelvic and menstrual pain diagnosis",
-              "Headache diagnosis",
-              "Other painful condition diagnosis",
-              "Orofacial, ear, and/or temporomandibular disorder pain diagnosis",
-              "Fibromyalgia diagnosis",
-              "Average morphine milligram equivalents (MME) dose per day",
-              "Benzodiazepine dispensed in 6 months prior to randomization"
+
+# ====================================================
+# Spline-Based Likelihood Ratio Test Analysis
+# ====================================================
+
+# Fit full model with splines
+model_full_spline <- lmer(
+  PEGS ~ INTERVENTION * (ns(AGE, df = 4) + ns(BMI, df = 4) + ns(BL_avg_daily, df = 4) +
+                           FEMALE + disable + Current_Smoke +
+                           Alcohol_Abuse + Drug_Abuse + Diabetes +
+                           CVD + Hypertension + Pulmonary + Dep_OR_Anx +
+                           pain1 + pain2 + pain3 + pain4 + pain5 + pain6 +
+                           pain7 + pain8 + pain9 + pain10 + pain11 +
+                           BL_benzo_flag) + (1 | CLUST),
+  data = data_12_cov
 )
-#print(xtable(data.frame(cov_names, p_vals_ind, p_vals_combined), digits=c(0,0,2,2)),include.rownames = FALSE)
+
+# Fit null model with splines
+model_null_spline <- lmer(
+  PEGS ~ INTERVENTION + ns(AGE, df = 4) + ns(BMI, df = 4) + ns(BL_avg_daily, df = 4) +
+    FEMALE + disable + Current_Smoke +
+    Alcohol_Abuse + Drug_Abuse + Diabetes +
+    CVD + Hypertension + Pulmonary + Dep_OR_Anx +
+    pain1 + pain2 + pain3 + pain4 + pain5 + pain6 +
+    pain7 + pain8 + pain9 + pain10 + pain11 + BL_benzo_flag +
+    (1 | CLUST),
+  data = data_12_cov
+)
+
+# Perform likelihood ratio test
+anova(model_full_spline, model_null_spline)
+
+# Stepwise selection to identify key predictors for adjustment in permutation test
+step(model_full_spline, direction = "backward", reduce.random = FALSE)
+
+
+
+# ====================================================
+# Ad-hoc analysis to detect source of heterogeneity
+# ====================================================
+
+# Scale continuous covariates
+data_12_cov$AGE_scale <- scale(data_12_cov$AGE)
+data_12_cov$BMI_scale <- scale(data_12_cov$BMI)
+data_12_cov$BL_avg_daily_scale <- scale(data_12_cov$BL_avg_daily)
+
+# ====================================================
+# Fit full model including all covariates with a random effect for clusters
+# ====================================================
+model_full_spline_scaled <- lmer(
+  PEGS ~ INTERVENTION * (ns(AGE_scale, df = 4) + ns(BMI_scale, df = 4) + ns(BL_avg_daily_scale, df = 4) +
+                           FEMALE + disable + Current_Smoke +
+                           Alcohol_Abuse + Drug_Abuse + Diabetes +
+                           CVD + Hypertension + Pulmonary + Dep_OR_Anx +
+                           pain1 + pain2 + pain3 + pain4 + pain5 + pain6 +
+                           pain7 + pain8 + pain9 + pain10 + pain11 +
+                           BL_benzo_flag) + (1 | CLUST),
+  data = data_12_cov
+)
+
+# Extract fixed effects and treatment effect estimates
+fixed_effects <- fixef(model_full_spline_scaled)
+intervention_effects <- fixed_effects[grep("INTERVENTION", names(fixed_effects))]
+
+# Construct model matrix for covariates
+X_mat <- model.matrix(~ ns(AGE_scale, df = 4) + ns(BMI_scale, df = 4) + ns(BL_avg_daily_scale, df = 4) +
+                        FEMALE + disable + Current_Smoke +
+                        Alcohol_Abuse + Drug_Abuse + Diabetes +
+                        CVD + Hypertension + Pulmonary + Dep_OR_Anx +
+                        pain1 + pain2 + pain3 + pain4 + pain5 + pain6 +
+                        pain7 + pain8 + pain9 + pain10 + pain11 + BL_benzo_flag,
+                      data = data_12_cov)
+
+# Compute the full model's estimated treatment effect
+full_treatment_effect <- mean(X_mat %*% intervention_effects)
+
+# ====================================================
+# Function to construct a reduced model formula excluding a covariate
+# ====================================================
+build_reduced_formula <- function(excluded_var, continuous_vars, categorical_vars, model_matrix = FALSE) {
+  continuous_terms <- sapply(
+    continuous_vars,
+    function(var) if (var == excluded_var) NULL else paste0("ns(", var, ", df = 4)")
+  )
+  categorical_terms <- sapply(
+    categorical_vars,
+    function(var) if (var == excluded_var) NULL else var
+  )
+  terms <- paste(c(continuous_terms, categorical_terms)[!sapply(c(continuous_terms, categorical_terms), is.null)], collapse = " + ")
+
+  if (!model_matrix) {
+    return(as.formula(paste("PEGS ~ INTERVENTION * (", terms, ") + (1 | CLUST)")))
+  } else {
+    return(as.formula(paste(" ~ ", terms)))
+  }
+}
+
+# ====================================================
+# Define covariates and separate into continuous and categorical groups
+# ====================================================
+covariates <- c("AGE_scale", "FEMALE", "disable", "Current_Smoke", "BMI_scale",
+                "Alcohol_Abuse", "Drug_Abuse", "Diabetes", "CVD",
+                "Hypertension", "Pulmonary", "Dep_OR_Anx",
+                paste0("pain", 1:11), "BL_avg_daily_scale", "BL_benzo_flag")
+
+continuous_vars <- paste0(c("AGE", "BMI", "BL_avg_daily"), "_scale")
+categorical_vars <- setdiff(covariates, continuous_vars)
+
+# Store treatment effects for reduced models
+results <- data.frame(Covariate = covariates, Delta = NA)
+
+# ====================================================
+# Loop through each covariate to fit reduced models
+# ====================================================
+for (covariate in covariates) {
+  # Build the reduced formula
+  reduced_formula <- build_reduced_formula(
+    excluded_var = covariate,
+    continuous_vars = continuous_vars,
+    categorical_vars = categorical_vars
+  )
+
+  # Build the reduced model matrix formula
+  model_matrix_reduced_formula <- build_reduced_formula(
+    excluded_var = covariate,
+    continuous_vars = continuous_vars,
+    categorical_vars = categorical_vars,
+    model_matrix = TRUE
+  )
+
+  # Fit the reduced model
+  reduced_model <- lmer(reduced_formula, data = data_12_cov)
+
+  # Construct model matrix for the reduced model
+  X_mat = model.matrix(model_matrix_reduced_formula, data = data_12_cov)
+
+  # Extract fixed effects and treatment effect estimates
+  fixed_effects <- fixef(reduced_model)
+  intervention_effects <- fixed_effects[grep("INTERVENTION", names(fixed_effects))]
+
+  # Store results
+  results$Delta[results$Covariate == covariate] <- mean(X_mat %*% intervention_effects)
+}
+
+# ====================================================
+# Compute percent difference in treatment effect estimation
+# ====================================================
+results$Original_Delta <- full_treatment_effect
+results$Diff <- results$Original_Delta - results$Delta
+results$Percent_Difference <- (results$Original_Delta - results$Delta) / results$Original_Delta * 100
+
+# Sort results by percent difference in descending order
+results_sorted <- results %>%
+  arrange(desc(abs(Percent_Difference)))
+
+# Transform data for visualization
+results_sorted_long <- results_sorted %>%
+  pivot_longer(cols = c(Delta), names_to = "Type", values_to = "Delta")
+
+results_sorted_long$Covariate <- factor(results_sorted_long$Covariate,
+                                        levels = covariates,
+                                        labels = cov_names)
+
+results_sorted$Covariate <- factor(results_sorted$Covariate,
+                                   levels = covariates,
+                                   labels = cov_names)
+
+# ====================================================
+# Create bar plot to visualize impact of variable exclusion
+# ====================================================
+ggplot(results_sorted_long, aes(x = reorder(Covariate, Delta), y = Delta)) +
+  geom_bar(stat = "identity", position = "dodge", width = 0.7) +
+  geom_hline(yintercept = full_treatment_effect, linetype = "dashed", color = "gray", size = 0.8) +
+  geom_text(
+    aes(label = sprintf("%.1f%%", results_sorted$Percent_Difference[match(Covariate, results_sorted$Covariate)]),
+        y = 0.02),  # Offset text above bars
+    position = position_dodge(width = 0.7),
+    vjust = 0.5,
+    size = 3
+  ) +
+  labs(
+    title = "Impact of Variable Exclusion on Estimated Treatment Effect",
+    subtitle = sprintf("Estimated Average Treatment Effect Using All Variables: %.2f (Dashed Gray Line)", full_treatment_effect),
+    y = "Estimated Leave-One-Out Average Treatment Effect",
+    x = ""
+  ) +
+  scale_fill_manual(values = c("blue")) +
+  theme_minimal() +
+  coord_flip() +
+  theme(legend.position = "none")
+
+
---
+++ simulation_study.R
@@ -0,0 +1,210 @@
+# ====================================================
+# Simulation Parameters and Setup (Table 4 Rows 1-8)
+# ====================================================
+
+# Capture command-line arguments
+params <- commandArgs(trailingOnly = TRUE)
+
+# Define simulation parameters from command-line input
+k_max <- as.numeric(params[1])  # Maximum number of clusters
+n_per <- as.numeric(params[2])  # Number of individuals per cluster
+R <- as.numeric(params[3])  # Number of permutations for permutation test
+Delta.sd.scale <- as.numeric(params[4])  # Scaling factor for SD of treatment effect heterogeneity
+index <- as.numeric(params[5])  # Index identifier for batch processing
+
+# Default values for additional simulation parameters
+Delta.mean <- 1  # Mean treatment effect
+p_trt <- 0.5  # Probability of receiving treatment
+icc_x <- 0.01  # Intra-cluster correlation for covariates
+icc_y <- 0.1  # Intra-cluster correlation for outcome
+sigma_eps_sq <- 0.1  # Variance of residual errors
+mu_x <- 0  # Mean of covariates
+sigma_x_sq <- 1  # Variance of covariates
+sd_n_per <- 0  # Standard deviation for per-cluster sample size
+gamma <- 0.0001  # Regularization parameter
+grid.size <- 151  # Grid size for numerical integration
+
+# Load necessary helper functions
+source("permutation_test_crt.R")
+
+# ====================================================
+# Function: set_up_trial
+# Generates a simulated cluster randomized trial dataset
+# ====================================================
+
+set_up_trial <- function(k_max, n_per, sd_n_per, p_trt, icc_y, icc_x, mu_x, sigma_x_sq, sigma_eps_sq) {
+
+  # Generate cluster sizes with normally distributed variability
+  count <- rnorm(k_max, n_per, sd_n_per)
+
+  # Create treatment assignment for clusters
+  freq_table <- data.frame(
+    k = rep(1:k_max),  # Cluster IDs
+    W = sample(rep(0:1, each = p_trt * k_max))  # Assign treatment/control
+  )
+
+  # Assign the generated cluster sizes to the frequency table
+  freq_table$count <- count
+
+  # Expand the dataset to include individuals within each cluster
+  data <- freq_table[rep(1:nrow(freq_table), freq_table[["count"]]), -3]
+
+  # ====================================================
+  # Generate Individual-Level Covariates with Cluster Effects
+  # ====================================================
+
+  # Generate cluster-level random effects for the first covariate (X_1)
+  nu_k <- rnorm(k_max, 0, sqrt(icc_x * sigma_x_sq))
+
+  # Assign individual-level values for X_1, incorporating cluster effects
+  data$X_1 <- mu_x + apply(data, 1, function(x) {
+    nu_k[as.numeric(x["k"])]  # Use cluster random effect
+  }) + rnorm(nrow(data), 0, sqrt((1 - icc_x) * sigma_x_sq))  # Add individual noise
+
+  # Generate cluster-level random effects for the second covariate (X_2)
+  nu_k_2 <- rnorm(k_max, 0, sqrt(icc_x * sigma_x_sq))
+
+  # Assign individual-level values for X_2, incorporating cluster effects
+  data$X_2 <- mu_x + apply(data, 1, function(x) {
+    nu_k_2[as.numeric(x["k"])]  # Use second cluster random effect
+  }) + rnorm(nrow(data), 0, sqrt((1 - icc_x) * sigma_x_sq))  # Add individual noise
+
+  return(data)
+}
+
+
+# ====================================================
+# Function: one_iter
+# Simulates a single iteration of the trial under different scenarios
+# ====================================================
+
+one_iter <- function(k_max, n_per, sd_n_per, p_trt, scenario, Delta.mean,
+                     Delta.sd.scale, icc_y, icc_x, mu_x, sigma_x_sq, sigma_eps_sq) {
+
+  # Compute cluster-level variance based on intra-cluster correlation
+  sigma_cl_sq <- sigma_eps_sq * (icc_y / (1 - icc_y))
+
+  # Generate cluster-level random effects
+  group_ranef <- rnorm(k_max, 0, sqrt(sigma_cl_sq))
+
+  # Generate trial data
+  data <- set_up_trial(k_max, n_per, sd_n_per, p_trt, icc_y, icc_x, mu_x, sigma_x_sq, sigma_eps_sq)
+
+  # Rename variables for clarity
+  names(data) <- c("k", "W", "X_1", "X_2")
+
+  # Generate observed outcomes with cluster-level random effects
+  data$Y_obs <- 2 * data$X_1 + 1.5 * data$X_2 +
+    group_ranef[data$k] +
+    rnorm(nrow(data), 0, sqrt(sigma_eps_sq))
+
+  # ====================================================
+  # Apply Different Scenarios to Modify Treatment Effect
+  # ====================================================
+
+  if (scenario == 1) {
+    # Scenario 1: Null hypothesis holds (Type I error test)
+    data$Y_obs <- data$Y_obs + Delta.mean * data$W
+  } else if (scenario == 2) {
+    # Scenario 2: Linear treatment effect modification
+    Deltas = Delta.sd.scale * (0.28 * data$X_1 + 0.3 * data$X_2)
+    data$Y_obs <- data$Y_obs + Delta.mean * data$W + Deltas * data$W
+    Delta.mean <- mean(Delta.mean + Deltas[data$W==1])
+   } else if (scenario == 3) {
+    # Scenario 3: Non-linear (cosine transformation) treatment effect modification
+    Deltas <- Delta.sd.scale * (0.6 * cos(6 * data$X_1 / pi))
+    data$Y_obs <- data$Y_obs + Delta.mean * data$W + Deltas * data$W
+    Delta.mean <- mean(Delta.mean + Deltas[data$W==1])
+  } else if (scenario == 4) {
+    # Scenario 4: Quadratic treatment effect modification
+    Deltas <- (0.1 * data$X_1 + 2 * data$X_1^2 + data$X_2^2) * Delta.sd.scale * 0.2 / sqrt(0.1 + 2)
+    data$Y_obs <- data$Y_obs + Delta.mean * data$W + Deltas * data$W
+    Delta.mean <- mean(Delta.mean + Deltas[data$W == 1])
+  }
+
+  # ====================================================
+  # Perform Permutation Tests
+  # ====================================================
+
+  # Unadjusted permutation test
+  perm.test.unadj <- runPermutationTest(data$Y_obs, data$W, data$k, R = R, truth = Delta.mean)
+
+  # Adjusted permutation test (controlling for covariates)
+  perm.test.adj <- runPermutationTest(data$Y_obs, data$W, data$k,
+                                      data[, c("X_1", "X_2")], X.bin = NA,
+                                      adj = TRUE, R = R, truth = Delta.mean)
+
+  # ====================================================
+  # Likelihood Ratio Tests for Interaction Effects
+  # ====================================================
+
+  # Standard linear models
+  no_interaction_model <- lmer(Y_obs ~ W + X_1 + X_2 + (1 | k), data = data, REML = FALSE)
+  interaction_model <- lmer(Y_obs ~ W * (X_1 + X_2) + (1 | k), data = data, REML = FALSE)
+
+  # Compute p-value from likelihood ratio test
+  p_value_model <- anova(interaction_model, no_interaction_model)[2, 8]
+
+  # Spline-based interaction models
+  spline_no_interaction_model <- lmer(Y_obs ~ W + ns(X_1, df = 4) + ns(X_2, df = 4) + (1 | k), data = data, REML = FALSE)
+  spline_interaction_model <- lmer(Y_obs ~ W * ns(X_1, df = 4) + W * ns(X_2, df = 4) + (1 | k), data = data, REML = FALSE)
+
+  # Compute p-value from spline-based likelihood ratio test
+  p_value_spline_model <- anova(spline_interaction_model, spline_no_interaction_model)[2, 8]
+
+  # ====================================================
+  # Return Results
+  # ====================================================
+
+  returned <- data.frame(
+    k_max, n_per, sd_n_per, p_trt, scenario, Delta.mean, Delta.sd.scale, gamma,
+    icc_y, icc_x, mu_x, sigma_x_sq, sigma_eps_sq,
+    p_value_ci = perm.test.unadj$p.value.ci,
+    p_value_pi = perm.test.unadj$p.value.pi,
+    p_value_truedelta = perm.test.unadj$p.value.truth,
+    p_value_ci_residuals = perm.test.adj$p.value.ci,
+    p_value_pi_residuals = perm.test.adj$p.value.pi,
+    p_value_truedelta_residuals = perm.test.adj$p.value.truth,
+    p_value_lr_test = p_value_model,
+    p_value_spline_model
+  )
+
+  return(returned)
+}
+
+# ====================================================
+# Function: one_run
+# Runs multiple scenarios in a single execution
+# ====================================================
+
+one_run <- function() {
+  ret <- foreach(scenario = 1:4, .combine = rbind) %do%
+    one_iter(k_max, n_per, sd_n_per, p_trt, scenario, Delta.mean,
+             Delta.sd.scale, icc_y, icc_x, mu_x, sigma_x_sq, sigma_eps_sq)
+  return(ret)
+}
+
+# ====================================================
+# Parallel Execution Setup
+# ====================================================
+
+registerDoParallel(cores = 10)
+
+# Run simulations in parallel
+out <- foreach(iter = 1:10, .combine = rbind) %dopar% one_run()
+
+# Store execution time
+end_time <- Sys.time()
+out$time <- difftime(end_time, start_time, units = "mins")
+out$R <- R
+
+# ====================================================
+# Save Output Results
+# ====================================================
+
+setwd("results")
+
+out.name <- paste0("splines_p3_power_sims_", n_per, "_", k_max, "_", R, "_",
+                   Delta.sd.scale, "_", index, ".csv")
+
+write.csv(out, out.name, row.names = FALSE)
